c7ea2c0357d79022f3a54e0411947a8a07a59972
guilherme-d-s <gds.z_@hotmail.com>
2023-02-23 21:47:58 -0300

    Adicionado exercicio1 da pasta exercicios3

---
+++ exercicios3/exercicio1/senha/senha/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace senha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Escreva um programa que repita a leitura de uma senha até que ela seja válida.Para cada leitura de senha
+            incorreta informada, escrever a mensagem "Senha Invalida".Quando a senha for informada corretamente deve ser
+            impressa a mensagem "Acesso Permitido" e o algoritmo encerrado.Considere que a senha correta é o valor 2002.*/
+
+            Console.Write("Digite sua senha: ");
+
+            double pass = double.Parse(Console.ReadLine());
+
+            double cpass = 2002;
+            while (pass != cpass)
+            {
+                Console.WriteLine("Senha Invalida");
+                Console.Write("Digite sua senha: ");
+                pass = double.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Acesso Permitido.");
+        }
+    }
+}
